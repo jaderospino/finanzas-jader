1630bf2aa1d03d8108f6615f0a17d71dfefd8e53
Jader Ospino <dayanjor2216@gmail.com>
2025-09-25 11:10:56 -0500

    feat: Convertir la aplicación a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 5173 }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Con esta configuración, el plugin también generará el archivo manifest.json por ti.
+      manifest: {
+        name: 'Finanzas - Jader',
+        short_name: 'Finanzas',
+        description: 'Aplicación de seguimiento de finanzas personales.',
+        theme_color: '#ffffff', // Color de la barra de título en la app
+        background_color: '#1e293b', // Color de fondo para la pantalla de carga
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // Ícono adaptable para diferentes formas en Android
+          }
+        ]
+      }
+    })
+  ],
 })
